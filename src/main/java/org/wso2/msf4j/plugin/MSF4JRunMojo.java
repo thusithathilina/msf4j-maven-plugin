3eea5af9d493a05f92fb2576cef79f453f899e90
Thusitha Dayaratne <thusithad@wso2.com>
2017-09-08 09:41:14 +0530

    Clean up the code and add licence

--- src/main/java/org/wso2/msf4j/plugin/MSF4JRunMojo.java
+++ src/main/java/org/wso2/msf4j/plugin/MSF4JRunMojo.java
@@ -1,5 +1,20 @@
-package org.wso2.msf4j.plugin;
+/*
+ *  Copyright (c) 2017, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
 
+package org.wso2.msf4j.plugin;
 
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
@@ -12,7 +27,6 @@ import org.apache.maven.plugins.annotations.Parameter;
 import org.apache.maven.project.MavenProject;
 
 import java.io.BufferedReader;
-import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
@@ -24,15 +38,6 @@ import java.io.InputStreamReader;
 @Mojo(name = "run", defaultPhase = LifecyclePhase.PACKAGE)
 @Execute(phase = LifecyclePhase.PACKAGE)
 public class MSF4JRunMojo extends AbstractMojo {
-
-    /**
-     * Location of the file.
-     */
-    @Parameter(defaultValue = "${project.build.directory}", property = "outputDir", required = true)
-    private File outputDirectory;
-
-    @Parameter(property = "microservice.mainClass", required = true)
-    private String mainClass;
 
     @Parameter(defaultValue = "${project}", readonly = true, required = true)
     private MavenProject project;
